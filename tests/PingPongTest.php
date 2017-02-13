fdb33007534ed122d1938d205893bd63cfe08927
Benjamin T. Seaver and Sarah Leahy <sarah@>
2017-02-13 11:14:40 -0800

    phase 3 - add second test case

--- tests/PingPongTest.php
+++ tests/PingPongTest.php
@@ -18,6 +18,20 @@
             //Assert
             $this->assertEquals(1, $result);
         }
+
+        function test_calculateExceptions_n3()
+        {
+            //Arrange
+            $test_expections = array(array(3, 'Ping'), array(5, 'Pong'), array(15, 'Ping-Pong'));
+            $test_PingPong = new PingPong($test_expections);
+            $input = 3;
+
+            //Act
+            $result = $test_PingPong->calculateException($input);
+
+            //Assert
+            $this->assertEquals('Ping', $result);
+        }
     }
 
 ?>
